e24a9835fbb8bc92ac17958625059c66ab724d54
Apra487 <apra487@gmail.com>
2023-07-04 14:12:36 +0530

    feat: push commit to remote repository 🎉

--- utils/git.js
+++ utils/git.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const simpleGit = require('simple-git');
+require('dotenv').config();
 
 // ! need to refactor this ! this is a hacky solution ! To create a new directory named 'DailyCodingProblem' here
 // Check if the directory already exists
@@ -15,11 +16,13 @@ const git = simpleGit('./DailyCodingProblem/', { binary: 'git' }).clean(
 );
 
 git.init();
+git.addRemote('origin', process.env.GIT_REMOTE_URL);
 
 async function gitCommit(commitMsg) {
   try {
     await git.add('./*');
     await git.commit(commitMsg);
+    await git.push('origin', 'main');
   } catch (e) {
     console.error(e);
   }
